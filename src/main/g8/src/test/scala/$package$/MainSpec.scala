39c827b2cfda761ae56289df11c54ad82c5f49b9
lemastero <piotr.paradzinski@iohk.io>
2021-04-07 22:30:32 +0200

    use : syntax in test fix #1

--- src/main/g8/src/test/scala/$package$/MainSpec.scala
+++ src/main/g8/src/test/scala/$package$/MainSpec.scala
@@ -4,11 +4,10 @@ import zio.test.Assertion.equalTo
 import zio.test.junit.JUnitRunnableSpec
 import zio.test.{ assert, suite, test }
 
-object MainSpec extends JUnitRunnableSpec {
+object MainSpec extends JUnitRunnableSpec:
 
   def spec = suite("Test environment")(
     test("expect call with input satisfying assertion") {
       assert(40 + 2)(equalTo(42))
     }
   )
-}
