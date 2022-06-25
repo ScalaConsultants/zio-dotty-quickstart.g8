e9c6374b49bf722b87f123b4622e22028c1eb3b4
lemastero <piotr.paradzinski@iohk.io>
2022-06-25 21:56:52 +0200

    sbt scalafmt

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,15 +1,15 @@
 package $package$
 
-import zio.{ ZIOAppDefault, Task, ZIO, Console }
-import zio.Console.{ ConsoleLive, printLine, readLine }
+import zio.{ZIOAppDefault, Task, ZIO, Console}
+import zio.Console.{ConsoleLive, printLine, readLine}
 
 object Main extends ZIOAppDefault:
 
-  def app(c : Console): Task[Unit] =
+  def app(c: Console): Task[Unit] =
     for
-      _    <- printLine("What is your name?")
+      _ <- printLine("What is your name?")
       name <- readLine
-      out  <- printLine(s"Hello $name!")
+      out <- printLine(s"Hello $name!")
     yield out
 
   def run = app(ConsoleLive)
