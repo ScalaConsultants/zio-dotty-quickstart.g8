fdf1c1c8484fcf6660c2f766a8bbbcc33d8c9557
lemastero <piotr.paradzinski@iohk.io>
2022-06-25 21:37:12 +0200

    remove ZEnv

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,18 +1,15 @@
 package $package$
 
-import java.io.IOException
-
-import zio.{  ZIOAppDefault, ExitCode, URIO, ZEnv, ZIO, Console }
-import zio.Console.{ printLine, readLine }
+import zio.{ ZIOAppDefault, Task, ZIO, Console }
+import zio.Console.{ ConsoleLive, printLine, readLine }
 
 object Main extends ZIOAppDefault:
 
-  val app: ZIO[Console, IOException, Unit] =
+  def app(c : Console): Task[Unit] =
     for
       _    <- printLine("What is your name?")
       name <- readLine
-      out  <- printLine(s"Hello \$name!")
+      out  <- printLine(s"Hello $name!")
     yield out
 
-  def run =
-    app.exitCode
+  def run = app(ConsoleLive)
