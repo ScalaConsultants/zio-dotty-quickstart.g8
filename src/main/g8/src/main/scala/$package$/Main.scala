90636ad06d955b20412ece9d5d045305555f1292
lemastero <piotr.paradzinski@iohk.io>
2022-06-26 13:34:29 +0200

    add missing c. and restore escape at $

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,15 +1,15 @@
 package $package$
 
-import zio.{ZIOAppDefault, Task, ZIO, Console}
-import zio.Console.{ConsoleLive, printLine, readLine}
+import zio.{ZIOAppDefault, Task, Console}
+import zio.Console.ConsoleLive
 
 object Main extends ZIOAppDefault:
 
   def app(c: Console): Task[Unit] =
     for
-      _ <- printLine("What is your name?")
-      name <- readLine
-      out <- printLine(s"Hello $name!")
-    yield out
+      _    <- c.printLine("What is your name?")
+      name <- c.readLine
+      _    <- c.printLine(s"Hello \$name!")
+    yield ()
 
   def run = app(ConsoleLive)
